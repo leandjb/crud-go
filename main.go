20a83086d1931a942f40ee67e0133b72500fb968
leandjb <leandjb@gmail.com>
2024-08-07 19:14:37 -0500

    added UPDATE for user CRUD

--- main.go
+++ main.go
@@ -1,47 +1,89 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"os"
 	"bufio"
-	"strconv"
+	"fmt"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 var reader *bufio.Reader
 
 type User struct {
-	id int
+	id       int
 	username string
-	email string
-	age int
+	email    string
+	age      int
 }
 
 var id int
 var users map[int]User
 
-
 func eliminarUsuario() {
+	clearConsole()
+
+	fmt.Println("> Ingrese ID del usuario a eliminar:")
+	id, err := strconv.Atoi(readLine())
+
+	if err != nil {
+		panic("MSG: No es posible convertir string a un entero.")
+	}
+
+	if _, ok := users[id]; ok {
+		delete(users, id)
+	} else {
+			fmt.Println("> ID no encontrado.")
+	}
+
 	fmt.Println("MSG: Usuario eliminado.")
-	
 }
 
 func modificarUsuario() {
+	clearConsole()
+	fmt.Println("> Ingrese ID del usuario a modificar:")
+	id, err := strconv.Atoi(readLine())
+	if err != nil {
+		panic("MSG: No es posible convertir string a un entero.")
+	}
+
+	if _, ok := users[id]; ok {
+		fmt.Println("> Ingrese nuevo username:")
+		usernameNew := readLine()
+		usernameNew = strings.ToUpper(usernameNew)
+
+		fmt.Println("> Ingrese nuevo email:")
+		emailNew := readLine()
+		emailNew = strings.ToLower(emailNew)
+
+		fmt.Println("> Ingrese una nueva edad: ")
+		ageNew, err := strconv.Atoi(readLine())
+		if err != nil {
+			panic("MSG: No es posible convertir string a un entero.")
+		}
+
+		user := User{id, usernameNew, emailNew, ageNew}
+		users[id] = user
+	} else {
+		fmt.Println("> ID no encontrado.")
+	}
+
+
+
 	fmt.Println("MSG: Usuario actualizado exitosamente.")
-	
 }
 
 func listarUsuarios() {
 	clearConsole()
 	fmt.Println("*** Lista de Usuarios ***")
-	
-	for id, user := range users{
+
+	for id, user := range users {
 		fmt.Println(id, "-", user.username)
 	}
 
 	fmt.Println("\n")
-	
+
 }
 
 func crearUsuario() {
@@ -49,16 +91,16 @@ func crearUsuario() {
 	fmt.Println("> Ingrese un valor para username:")
 	username := readLine()
 	username = strings.ToUpper(username)
-	
+
 	fmt.Println("> Ingrese un valor para email:")
 	email := readLine()
 	email = strings.ToLower(email)
-	
+
 	fmt.Println("> Ingrese un valor para edad: ")
 	age, err := strconv.Atoi(readLine())
 
 	if err != nil {
-		fmt.Println("MSG: No es posible convertir string a un entero.")
+		panic("MSG: No es posible convertir string a un entero.")
 	}
 
 	id++
@@ -79,7 +121,7 @@ func readLine() string {
 	}
 }
 
-func clearConsole()  {
+func clearConsole() {
 	cmd := exec.Command("clear")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
@@ -90,33 +132,33 @@ func main() {
 	var option string
 	users = make(map[int]User)
 	reader = bufio.NewReader(os.Stdin)
-	
-	for{
+
+	for {
 		fmt.Println("-------- MENU --------")
-		fmt.Println("A. Crear usuario")
-		fmt.Println("B. Listar usuarios")
-		fmt.Println("C. Modificar usuario")
-		fmt.Println("D. Eliminar usuario")
-		fmt.Println("E. Salir")
+		fmt.Println("1. Crear usuario")
+		fmt.Println("2. Listar usuarios")
+		fmt.Println("3. Modificar usuario")
+		fmt.Println("4. Eliminar usuario")
+		fmt.Println("5. Salir")
 		fmt.Println("______________________")
-	
+
 		fmt.Println("> Ingresa una opcion:")
-		option = readLine()	
-		
-		if option == "salir" || option == "e"{
+		option = readLine()
+
+		if option == "salir" || option == "5" {
 			break
 		}
-	
+
 		switch option {
-		case "a", "crear":
+		case "1", "crear":
 			crearUsuario()
-		case "b", "listar":
+		case "2", "listar":
 			listarUsuarios()
-		case "c", "modificar":
+		case "3", "modificar":
 			modificarUsuario()
-		case "d", "eliminar":
+		case "4", "eliminar":
 			eliminarUsuario()
-	
+
 		default:
 			fmt.Println("MSG: Opcion invalida.")
 		}
